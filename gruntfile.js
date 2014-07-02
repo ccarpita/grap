c24ad3902e4801ae29e393e7840145b72a337fcf
Chris Carpita <ccarpita@gmail.com>
2014-07-02 00:03:40 -0400

    Add coveralls, fix linting
    Change-Id: I926efb65972d83561bf90c79ff733a666b245351

--- gruntfile.js
+++ gruntfile.js
@@ -4,10 +4,15 @@ module.exports = function(grunt) {
   var exec = require('child_process').exec;
   var mochaArgs = ' ' + (process.env.MOCHA_ARGS || '');
 
+  var execTest = './node_modules/.bin/mocha -R spec' + mochaArgs;
+  var execCoverage = './node_modules/.bin/istanbul cover ' +
+      './node_modules/.bin/_mocha -- ' +
+      '-u exports test/unit -R spec' + mochaArgs;
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
       options: {
         globals: {
           module: true,
@@ -20,15 +25,18 @@ module.exports = function(grunt) {
       tasks: ['jshint', 'exec:unitTest']
     },
     exec: {
-      unitTest: './node_modules/.bin/mocha -R spec' + mochaArgs,
-      coverage: './node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha -- -u exports test/unit -R spec' + mochaArgs,
+      unitTest: execTest,
+      coverage: execCoverage,
+      coverageBuild: execCoverage + ' && cat ./coverage/lcov.info | ' +
+          './node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage',
       debug: 'npm-debug _mocha -s 600000 -s 600000' + mochaArgs
     }
   });
 
   grunt.registerTask('default', ['jshint', 'exec:unitTest']);
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('test', ['lint', 'exec:coverage']);
+  grunt.registerTask('coverage', ['exec:coverage']);
+  grunt.registerTask('test', ['lint', 'exec:coverageBuild']);
 
   grunt.registerTask('debug', ['exec:debug']);
 };
